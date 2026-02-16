b5068f7c3b0442330862a03d6117708382d93467
Daniel Starnes <dstarnes261@gmail.com>
2026-02-16 15:27:51 -0600

    added general ui scheme for home page implemented Order class

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Daniel Starnes on 2/16/26.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
